8a4a238555f759f59b87cc0c1af8d494b34111a6
Ryoji kitano <ryojikitanodev@gmail.com>
2020-01-24 16:40:56 -0300

    inicio do projeto

---
+++ BlogPessoal/BlogPessoal.Web/Data/Contexto/BlogPessoalContexto.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using BlogPessoal.Web.Models;
+using BlogPessoal.Web.Data.Mapeamento;
+
+namespace BlogPessoal.Web.Data.Contexto
+{
+    public class BlogPessoalContexto : DbContext
+    {
+        public BlogPessoalContexto()
+            : base(typeof(BlogPessoalContexto).Name)
+        {
+
+        }
+
+        public DbSet<CategoriaDeArtigo> CategoriasDeArtigo { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoriaDeArtigoMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
